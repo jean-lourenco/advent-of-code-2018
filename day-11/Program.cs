b6350a73535cc3eaa07861ce643d94ead16fd32e
Jean Lourenço <jeancarlos.bnu@gmail.com>
2018-12-11 23:01:34 -0200

    Day 11 completed

--- day-11/Program.cs
+++ day-11/Program.cs
@@ -13,15 +13,15 @@ namespace day_11
             Console.WriteLine($"Part1 - The X,Y coordinates of the top-left cell: {x1},{y1}");
 
             var (x2, y2, size) = Part2(cells);
-            //System.Diagnostics.Debug.Assert(x2 == 0 && y2 == 0 && size == 0);
+            System.Diagnostics.Debug.Assert(x2 == 90 && y2 == 169 && size == 15);
             Console.WriteLine($"Part2 - The X,Y,Size coordinates of the top-left cell: {x2},{y2},{size}");
         }
 
         public static (int, int) Part1(out int[,] cells)
         {
             var serial = 7689;
+            (int X, int Y, int Value) bestArea = default;
             cells = new int[300, 300];
-            var sumRelations = new Dictionary<(int, int), int>();
 
             for (int y = 0; y < cells.GetUpperBound(0); y++)
             {
@@ -42,21 +42,22 @@ namespace day_11
                     if (x + 2 > 299 || y + 2 > 299)
                         continue;
 
-                    sumRelations.Add((x, y),
-                        cells[y, x] + cells[y + 1, x + 1] + cells[y + 2, x + 2]
+                    var value = cells[y, x] + cells[y + 1, x + 1] + cells[y + 2, x + 2]
                         + cells[y + 1, x] + cells[y + 2, x] + cells[y + 2, x + 1]
-                        + cells[y, x + 1] + cells[y, x + 2] + cells[y + 1, x + 2]);
+                        + cells[y, x + 1] + cells[y, x + 2] + cells[y + 1, x + 2];
+
+                    if (value > bestArea.Value)
+                        bestArea = (x, y, value);
                 }
             }
 
-            var (x1, y1) = sumRelations.OrderBy(s => s.Value).Last().Key;
-            return (x1 + 1, y1 + 1);
+            return (bestArea.X + 1, bestArea.Y + 1);
         }
 
         public static (int, int, int) Part2(int[,] cells)
         {
-            var sumRelations = new Dictionary<(int, int, int), int>();
-            Console.WriteLine(cells[0, 289]);
+            (int X, int Y , int Size, int Value) bestArea = default;
+
             for (var y = 0; y < cells.GetUpperBound(0); y++)
             {
                 for (var x = 0; x < cells.GetUpperBound(1); x++)
@@ -64,28 +65,28 @@ namespace day_11
                     var size = 0;
                     var value = 0;
 
-                    for (int xSlice = x, ySlice = y; xSlice < 299 && ySlice < 299; ySlice = xSlice += 1)
+                    for (int xSlice = x, ySlice = y; xSlice < 299 && ySlice < 299;)
                     {
                         value += cells[ySlice, xSlice];
 
-                        for (int x1 = x; x1 < ySlice; x1++)
+                        for (int x1 = x; x1 < xSlice; x1++)
                             value += cells[ySlice, x1];
 
-                        for (int y1 = y; y1 < xSlice; y1++)
+                        for (int y1 = y; y1 < ySlice; y1++)
                             value += cells[y1, xSlice];
 
                         size++;
-                        sumRelations.Add((x, y, size  * size), value);
-                    }
 
+                        if (value > bestArea.Value)
+                            bestArea = (x, y, size, value);
+
+                        ySlice++;
+                        xSlice++;
+                    }
                 }
             }
 
-            var (a,b,c) = sumRelations.OrderBy(d => d.Value).Last().Key;
-            Console.WriteLine($"{a+1},{b+1},{c}");
-
-            var (x2, y2, size2) = sumRelations.OrderBy(s => s.Value).Last().Key;
-            return (x2 + 1, y2 + 1, size2);
+            return (bestArea.X + 1, bestArea.Y + 1, bestArea.Size);
         }
     }
 }
